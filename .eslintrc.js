bfae19b55b608190b5023bcd9e1021dbec9a2da8
Vladislav <vidga471@gmail.com>
2023-10-02 19:50:00 +0300

    add eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+		'jest/globals': 'true',
+	},
+	extends: ['airbnb-base', 'prettier'],
+	overrides: [
+		{
+			env: {
+				node: true,
+				'jest/globals': true,
+			},
+			files: ['.eslintrc.{js,cjs}'],
+			parserOptions: {
+				sourceType: 'script',
+			},
+		},
+	],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	plugins: ['jest'],
+	rules: {
+		'no-plusplus': 'off',
+		'eol-last': 'off',
+		'max-len': ['error', 150, { ignoreUrls: true, ignoreComments: true }],
+		'no-tabs': 'off',
+		'no-alert': 'off',
+		'prefer-destructuring': 'off',
+		indent: [2, 'tab'],
+		'no-param-reassign': 'off',
+		'linebreak-style': 'off',
+		'no-unused-vars': 'off',
+		'import/prefer-default-export': 'off',
+	},
+};

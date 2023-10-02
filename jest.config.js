831edb79a9227e8e4c340d41299812825c7cd82e
Vladislav <vidga471@gmail.com>
2023-10-02 19:44:00 +0300

    add jest coverage

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+/** @type {import('jest').Config} */
+const config = { 
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  testEnvironment: "jsdom",
+  coverageThreshold:{
+  global: {
+  branches: 80,
+  functions: 80,
+  lines: 80,
+  statements: -10,
+        }
+    },    
+};
+
+module.exports = config;

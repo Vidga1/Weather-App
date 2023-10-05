a40ec42f7eeaaf2c9500d93bb7ba4b1426af6b1b
Vladislav <vidga471@gmail.com>
2023-10-05 22:11:09 +0300

    install webpack with examlple

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './sass/main.scss'
+import weather from './assets/weather.jpg'
+
+const mainImage = document.getElementById('mainImage')
+mainImage.src = weather
+
+const hello = require('./js/hello')
